7a997824a73149e21410cfb22bc8ecdb3491a6a3
JokerK9 <85178957+JokerK9@users.noreply.github.com>
2021-12-14 15:43:16 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='Trendings',
+      version='1.0',
+      description='Trending Distribution Package',
+      author='Daniel',
+      author_email='aluno81459@ipt.pt ',
+      url='https://github.com/',
+      packages=['Trendings'],
+     )
